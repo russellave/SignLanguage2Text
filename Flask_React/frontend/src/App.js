8473ed3503cd31607d13571007f17ff54c399455
Caroline Potts <crp41@duke.edu>
2020-04-20 17:24:40 -0400

    separate input v2w and w2s on same page, deleted trial page

--- Flask_React/frontend/src/App.js
+++ Flask_React/frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import HomePage from './pages/home_page/home_page.jsx';
-import TrialPage from './pages/trial_page/trial_page.jsx';
 import TranslationPage from './pages/translation_page/translation_page.jsx';
 import InfoPage from './pages/info_page/info_page.jsx';
 import GenTextPage from './pages/gen_text_page/gen_text_page.jsx';
@@ -15,7 +14,6 @@ class App extends Component {
             <div>
                 <Switch>
                     <Route exact={true} path='/' component={HomePage} />
-                    <Route exact={true} path='/trial' component={TrialPage} />
                     <Route exact={true} path='/translate' component={TranslationPage} />
                     <Route exact={true} path='/info' component={InfoPage} />
                     <Route exact={true} path='/gentext' component={GenTextPage} />
--- Flask_React/frontend/src/pages/translation_page/translation_page.jsx
+++ Flask_React/frontend/src/pages/translation_page/translation_page.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import NavBar from '../../components/navbar/navbar.jsx';
 import Vid2TextInput from '../../components/vid2text_input/vid2text_input.jsx';
+import W2SInput from '../../components/w2s_input/w2s_input.jsx';
 
 class TranslationPage extends Component {
 
@@ -9,6 +10,8 @@ class TranslationPage extends Component {
             <div>
                 <NavBar />
                 <Vid2TextInput/>
+                <br/>
+                <W2SInput/>
             </div>
         );
     }
